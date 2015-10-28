6e63aa8e2ec60b1d430954c4de456b3909888cc2
strangetype <macsimusfactorre@gmail.com>
2015-10-28 22:48:03 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var path = require('path');
+var webpack = require('webpack');
+var BowerWebpackPlugin = require('bower-webpack-plugin');
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var watch = require('gulp-watch');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+var node_modules = path.resolve(__dirname, 'node_modules');
+
+var config = require('./webpack.config.js');
+
+gulp.task('build', function () {
+  var compiler = webpack(config);
+  var assets = {
+    stylesheets: [
+      './bower_components/bootstrap-sass/assets/stylesheets/*',
+      './bower_components/bootstrap-sass/assets/stylesheets/*/**',
+    ]
+  };
+
+  for (var type in assets) {
+    gulp.src(assets[type]).pipe(gulp.dest('./static/assets/' + type + '/lib'));
+  }
+
+  compiler.run(function (err, stats) {
+    if (err) {
+      console.log('build fail:');
+      console.log(err)
+    } else if(stats.compilation.errors.length) {
+      console.log(stats.compilation.errors)
+    } else {
+      console.log('build completed');
+    }
+  });
+})
+
+gulp.task('watch', function () {
+  gulp.watch(['app/**'], ['build'])
+
+  gulp.watch('./assets/stylesheets/*.scss', ['styles']);
+  gulp.watch('./assets/stylesheets/**/*.scss', ['styles']);
+})
+
+gulp.task('styles', function () {
+  console.log('build css')
+  return sass('./assets/stylesheets/application.scss', {style: 'expanded'})
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./static/assets/stylesheets'));
+});
+
+gulp.task('server', function () {
+  connect.server({
+    port: 5000,
+    //https://github.com/AveVlad/gulp-connect/issues/27
+    //TODO: use plugin for proxy
+    middleware: function (connect, o) {
+      return [(function () {
+        var url = require('url');
+        var proxy = require('proxy-middleware');
+        var options = url.parse('http://localhost:9999/api');
+        options.route = '/api';
+        return proxy(options);
+      })()];
+    }
+  });
+})
+
+gulp.task('default', ['server', 'watch', 'build', 'styles']);
+
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+
+var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var BowerWebpackPlugin = require('bower-webpack-plugin');
+
+
+//'webpack/hot/dev-server',
+
+var config = {
+  context: path.join(__dirname, 'app'), // source directory
+  entry: ['./main'], // main.js file location
+  output: {
+    root: path.resolve(__dirname, 'app'),
+    path: path.join(__dirname, 'build') // output directory
+  },
+
+
+  resolve: {
+    root: path.resolve(__dirname, 'app'),
+    extensions: ['', '.js', '.jsx']
+  },
+  modulesDirectories: [ "node_modules", "bower_components"],
+  module: {
+    loaders: [
+      { test: /\.jsx$/, loader: 'jsx-loader?harmony' },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+      , { test: /react-intl\.js$/ , loader: "imports?this=>window,intl,react!exports?ReactIntl" }
+
+    ]
+  },
+  externals: {
+    //don't bundle the 'react' npm package with our bundle.js
+    //but get it from a global 'React' variable
+    'react': 'React'
+  },
+  plugins: [
+    new BowerWebpackPlugin({
+      modulesDirectories: ['bower_components'],
+      manifestFiles: ['bower.json', '.bower.json'],
+      includes: /.*/,
+      excludes: /.*\.less$/
+    })
+  ]
+};
+
+
+module.exports =  config;
