e3c5222748004b7270b4301cadf09d88ca25101b
strangetype <macsimusfactorre@gmail.com>
2015-10-30 21:17:19 +0200

    slider

--- app/layouts/Main.jsx
+++ app/layouts/Main.jsx
@@ -1,12 +1,15 @@
 var React = require('react');
 
+var _ = require('lodash');
+
 var Component = React.createClass({
 
     imagesCount: 9,
     ci1: 0,
-    ci2: 1,
+    ci2: 9,
     bkg1: null,
     bkg2: null,
+    activeImage: 0,
 
     componentDidMount: function() {
 
@@ -20,7 +23,6 @@ var Component = React.createClass({
 
         setInterval(()=>{
             this.bkgChange();
-            this.forceUpdate();
         },5000);
     },
 
@@ -28,39 +30,50 @@ var Component = React.createClass({
 
     bkgChange: function() {
         if (this._order===1) {
-            this.bkg1.style.opacity = 0;
-            this.bkg2.style.opacity = 1;
-
             this.ci2 = this.ci2+2;
             if (this.ci2>this.imagesCount) this.ci2 = 1;
-
+            this.activeImage = this.ci2;
+            this.forceUpdate();
+            this.bkg1.style.opacity = 0;
+            this.bkg2.style.opacity = 1;
             this._order = 2;
 
         } else {
-            this.bkg1.style.opacity = 1;
-            this.bkg2.style.opacity = 0;
-
             this.ci1 = this.ci1+2;
             if (this.ci1>this.imagesCount) this.ci1 = 0;
-
+            this.activeImage = this.ci1;
+            this.forceUpdate();
+            this.bkg1.style.opacity = 1;
+            this.bkg2.style.opacity = 0;
             this._order = 1;
         }
     },
 
     render: function() {
 
+        var points = [];
+
+        for (var i=0; i<=this.imagesCount; i++) {
+            points.push(i);
+        }
+
         return (
             <div className="layout-main">
                 <img ref="background" className="main-image" src ={"images/main"+this.ci1+".jpg"}/>
                 <img ref="_background" className="main-image" src ={"images/main"+this.ci2+".jpg"}/>
                 <div className = "left-menu">
+                    <img className="camera-logo" src="images/camera.png" />
+                    <div className="points">
+                        {points.map((p)=>{
+                            return <div className = {(p===this.activeImage) ? "point point--active" : "point"} ></div>
+                        })}
+                    </div>
                     <ul>
                         <li>Галлерея</li>
                         <li>Стоимость</li>
                         <li>Контакты</li>
                     </ul>
                 </div>
-                <img className="camera-logo" src="images/camera.png" />
                 <div className="photographer-title">
                     <h1 >Мария Тропина</h1>
                     <h2 >детский фотограф</h2>
