e86f1b55eca433612c25f84db93d8debd5c13323
Maxim Ostapov <maxim.ostapov@codex-soft.com>
2016-01-08 19:00:13 +0200

    conatcts

--- app/layouts/Contacts/Contacts.jsx
+++ app/layouts/Contacts/Contacts.jsx
@@ -34,39 +34,46 @@ var Services = React.createClass({
         return (
             <div className={layoutClass}>
                 <div className="content-container">
-                    <h2 className="contacts-title">Контакты</h2>
-                    <div className="contacts" >
-                        <div className="contact-item">
-                            <img src="images/gmail.png" />
-                            mtropinamoscow@gmail.com
+                    <div className="contacts-left-menu">
+                        <h2 className="contacts-title">Контакты</h2>
+                        <div className="contacts" >
+                            <div className="contact-item">
+                                <img src="images/mobile.png" />
+                                +7(903)596-05-12
+                            </div>
+                            <div className="contact-item">
+                                <img src="images/viber.png" />
+                                +7(903)506-05-12
+                            </div>
+                            <div className="contact-item">
+                                <img src="images/whatsapp.png" />
+                                +7(903)506-05-12
+                            </div>
+                            <div className="contact-item">
+                                <img src="images/gmail.png" />
+                                mtropinamoscow@gmail.com
+                            </div>
+                            <div className="contact-item">
+                                <a target="blank" href="https://www.instagram.com/mtropina/">
+                                    <img src="images/instagram.png" />
+                                    mtropina
+                                </a>
+                            </div>
                         </div>
-                        <div className="contact-item">
-                            <img src="images/mobile.png" />
-                            +7(903)596-05-12
-                        </div>
-                        <div className="contact-item">
-                            <img src="images/viber.png" />
-                            +7(903)506-05-12
-                        </div>
-                        <div className="contact-item">
-                            <img src="images/whatsapp.png" />
-                            +7(903)506-05-12
-                        </div>
-                        <div className="contact-item">
-                            <a target="blank" href="https://www.instagram.com/mtropina/">
-                                <img src="images/instagram.png" />
-                                mtropina
-                            </a>
+                        <hr></hr>
+                        <div className="contact-form">
+                            <form>
+                                <input type="text" placeholder="*имя" />
+                                <input type="text" placeholder="телефон" />
+                                <input type="text" placeholder="*Email" />
+                                <textarea  placeholder="*сообщение" />
+                                <input onClick={this.sendMessage} type="submit" />
+                            </form>
                         </div>
                     </div>
-                    <div className="contact-form">
-                        <form>
-                            <input type="text" placeholder="*имя" />
-                            <input type="text" placeholder="телефон" />
-                            <input type="text" placeholder="*Email" />
-                            <textarea  placeholder="*сообщение" />
-                            <input onClick={this.sendMessage} type="submit" />
-                        </form>
+                    <div className="contacts-right-menu">
+                        <h2>Мария Тропина</h2>
+                        <img className="contacts-owner-image" src = "http://cs618718.vk.me/v618718353/16a/D0Lf79XUZr0.jpg" />
                     </div>
                 </div>
             </div>
